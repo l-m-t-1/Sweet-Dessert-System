fac14b2272a08f6bde3042832dab4fc64e071519
l-m-t-1 <1944036288@qq.com>
2026-07-29 20:01:38 +0800

    feat: publish backend/src/main/java/com/sweet/dessertsystem

---
+++ backend/src/main/java/com/sweet/dessertsystem/DessertSystemApplication.java
@@ -0,0 +1,24 @@
+package com.sweet.dessertsystem;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@MapperScan({
+        "com.sweet.dessertsystem.mapper",
+        "com.sweet.dessertsystem.category",
+        "com.sweet.dessertsystem.dashboard"
+})
+@SpringBootApplication
+public class DessertSystemApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DessertSystemApplication.class,args);
+
+    }
+
+}
